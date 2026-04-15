0fdef2bc9120a9928d2fcb12b6d8e373e4f29d79
chaitanya pranav <101542958+chaitu43@users.noreply.github.com>
2026-04-15 15:57:45 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-
-import cv2
-
-model = YOLO("last.pt")
-
-results = model.predict(source="cracks.mp4", show=True)
-print(results)
